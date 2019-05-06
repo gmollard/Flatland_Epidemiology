e429cd7ee376754364ac25601f6d506e09c7d0c9
Guillaume Mollard <guillaume.mollard2@gmail.com>
2019-05-06 11:14:32 +0200

    resolve conflict

--- python/magent/builtin/config/agent_goal.py
+++ python/magent/builtin/config/agent_goal.py
@@ -3,7 +3,7 @@
 import magent
 
 
-def get_config(map_size, vaccine_reward=1):
+def get_config(map_size, vaccine_reward=1, view_radius=15):
     gw = magent.gridworld
 
     cfg = gw.Config()
@@ -18,13 +18,13 @@ def get_config(map_size, vaccine_reward=1):
          'view_range': gw.CircleRange(1), 'attack_range': gw.CircleRange(0),
          'damage': 0, 'step_recover': 0.0,
          'food_supply': 0, 'kill_supply': 8, 'kill_reward': 0, 'vaccine_range': gw.CircleRange(0),
-         'infection_radius': 2, 'infection_probability': 0.05
+         'infection_radius': 2, 'infection_probability': 0.02
          })
 
     tiger = cfg.register_agent_type(
         "tiger",
         {'width': 1, 'length': 1, 'hp': 10, 'speed': 1,
-         'view_range': gw.CircleRange(15), 'attack_range': gw.CircleRange(0), 'vaccine_range': gw.CircleRange(1),
+         'view_range': gw.CircleRange(view_radius), 'attack_range': gw.CircleRange(0), 'vaccine_range': gw.CircleRange(1),
          'damage': 1, 'step_recover': 0.0,
          'food_supply': 0, 'kill_supply': 0,
          'step_reward': -0.01, 'attack_penalty': 0.0, "vaccine_reward": vaccine_reward
